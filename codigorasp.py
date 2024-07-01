6ef322e7a723f4f4ac51f6175ffeab00a508a604
MinerUSMP3 <minergit2024@gmail.com>
2024-06-30 20:28:24 -0400

    se agrega el proyecto

---
+++ codigorasp.py
@@ -0,0 +1,17 @@
+wget https://github.com/demogorgonz/RaspberryPi-CPUminer/archive/refs/heads/master.zip
+
+unzip master.zip
+
+ls 
+
+cd RaspberryPi-CPUminer-master/
+
+ls 
+
+sudo bash build.sh
+
+./minerd
+
+./minerd -h
+
+./minerd -a scrypt -o stratum+tcp://us.litecoinpool.org:3333 -u walletpiusm.minerUSM -p 1234
